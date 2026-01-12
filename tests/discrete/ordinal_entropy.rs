d9378d7e898cb1ba879e13a47ec15ce0dae23760
Carlson Büth <commit@cbueth.de>
2026-01-12 15:16:27 +0100

    remove placeholder ordinal entropy test file
    Signed-off-by: Carlson Büth <commit@cbueth.de>

--- tests/discrete/ordinal_entropy.rs
+++
@@ -1,7 +0,0 @@
-// NOTE: Ordinal entropy tests were moved to tests/ordinal/ordinal_entropy.rs
-// This placeholder remains only to avoid breaking external references to file paths.
-
-#[test]
-fn ordinal_tests_relocated() {
-    assert!(true);
-}
